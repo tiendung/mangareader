baf89584d45b93ee7697c2ae0d7de79711f8be60
1 <a1@1s-MacBook.local>
2021-04-08 15:11:06 +0700

    move mangas gridview to a file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'mangas_provider.dart';
-import 'chapter_screen.dart';
 import 'manga_data.dart';
+import 'mangas_widget.dart';
 
 void main() {
   runApp(ProviderScope(
@@ -63,63 +62,7 @@ class MyHomePage extends ConsumerWidget {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: GridView.count(
-          padding: EdgeInsets.all(10.0),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 3,
-          childAspectRatio: 225.0 / 334.0, // itemWidth / itemHeight
-          children: mangas
-              .map((manga) => GestureDetector(
-                  onLongPress: () {
-                    print(manga.firstChapterUrl());
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        // builder: (_) => MangaScreen(manga: manga),
-                        builder: (_) => ChapterScreen(
-                            manga: manga, chapterUrl: manga.firstChapterUrl()),
-                      ),
-                    );
-                  },
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => ChapterScreen(
-                            manga: manga,
-                            chapterUrl: manga.defaultChapterUrl()),
-                      ),
-                    );
-                  },
-                  child: Container(
-                      padding: EdgeInsets.all(5.0),
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image:
-                              CachedNetworkImageProvider(manga.coverImageUrl),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      child: Opacity(
-                        opacity: 0.9,
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Text(
-                            manga.fullTitle(),
-                            // textAlign: TextAlign.center,
-                            // overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              // fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                              backgroundColor: Colors.black,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ))))
-              .toList(),
-        ),
+        child: MangasWidget(mangas: mangas),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _handleRefreshPressed,
